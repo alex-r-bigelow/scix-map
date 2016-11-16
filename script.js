c4918b40b3ac4175d07e0227cc90753d5a455452
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-11-16 10:37:50 -0700

    Restructure selections

--- script.js
+++ script.js
@@ -1,146 +1,137 @@
-/* globals d3, jQuery */
+/* globals d3 */
 
-let ANIMATION_SPEED = 300;
+var ANIMATION_SPEED = 300;
 
-function highlightAll (researchArea) {
-  let transition = d3.transition()
+var currentSelection = null;
+
+function updateHighlight (includeTrack, includeArea, includeTitle) {
+  var transition = d3.transition()
     .duration(ANIMATION_SPEED);
-  d3.select('svg').select('#Layer_2').selectAll('*')
-    .transition(transition)
-    .attr('opacity', d => {
-      if (!researchArea || d.Group === researchArea) {
-        return 1.0;
-      } else {
-        return 0.25;
-      }
-    });
-  d3.select('#titles').selectAll('div.poster')
-    .style('display', d => {
-      if (!researchArea || d.area === researchArea) {
-        return null;
-      } else {
-        return 'none';
-      }
-    });
+  // Update the tracks in the legend
   d3.select('#legend').selectAll('div.legendEntry')
     .transition(transition)
-    .style('opacity', d => {
-      if (!researchArea || d.key === researchArea) {
-        return 1.0;
-      } else {
-        return 0.25;
-      }
-    });
+    .style('opacity', function (d) { return includeTrack(d) ? 1.0 : 0.25; });
+  // Update the areas in the map
+  d3.select('svg').select('#Layer_2').selectAll('*')
+    .transition(transition)
+    .attr('opacity', function (d) { return includeArea(d) ? 1.0 : 0.25; });
+  // Update the list of titles
+  d3.select('#titles').selectAll('div.poster')
+    .style('display', function (d) { return includeTitle(d) ? null : 'none'; });
 }
-function loadSVG () {
-  return new Promise((resolve, reject) => {
-    jQuery.ajax({
-      url: 'map.svg',
-      dataType: 'text'
-    }).done(result => {
-      d3.select('#map').html(result);
-      resolve();
-    });
-  });
-}
-function getCSV (url) {
-  return new Promise((resolve, reject) => {
-    d3.csv(url, resolve);
-  });
-}
-function getTXT (url) {
-  return new Promise((resolve, reject) => {
-    jQuery.ajax({
-      url: url,
-      dataType: 'text',
-      success: resolve
-    });
-  });
-}
-
-Promise.all([getCSV('spaceAssignments.csv'), getCSV('researchAreas.csv'), getTXT('postertitles.txt'), loadSVG()]).then(results => {
-  // Create the color scale and the legend
-  let baseColor = {
-    'Reception': '#984ea3',
-    'Scientific Computing': '#ff7f00',
-    'Scientific Visualization': '#377eb8',
-    'Information Visualization': '#e41a1c',
-    'Biomedical Computation': '#ffff33',
-    'Imaging': '#4daf4a'
+function highlightTitle (title) {
+  currentSelection = currentSelection === title ? null : title;
+  var includeTrack = function (d) {
+    return currentSelection === null || window.areaToTrack[window.titleToArea[title]] === d;
   };
+  var includeArea = function (d) {
+    return currentSelection === null || window.titleToArea[title] === d.key;
+  };
+  var includeTitle = function (d) {
+    return currentSelection === null || title === d.Title;
+  };
+  updateHighlight(includeTrack, includeArea, includeTitle);
+}
+function highlightArea (area) {
+  currentSelection = currentSelection === area ? null : area;
+  var includeTrack = function (d) {
+    return currentSelection === null || window.areaToTrack[area] === d;
+  };
+  var includeArea = function (d) {
+    return currentSelection === null || area === d.key;
+  };
+  var includeTitle = function (d) {
+    return currentSelection === null || area === d.Area;
+  };
+  updateHighlight(includeTrack, includeArea, includeTitle);
+}
+function highlightTrack (track) {
+  currentSelection = currentSelection === track ? null : track;
+  var includeTrack = function (d) {
+    return currentSelection === null || track === d;
+  };
+  var includeArea = function (d) {
+    return currentSelection === null || track === d.value;
+  };
+  var includeTitle = function (d) {
+    return currentSelection === null || track === d.Track;
+  };
+  updateHighlight(includeTrack, includeArea, includeTitle);
+}
 
-  let legendEntries = d3.select('#legend').selectAll('div.legendEntry').data(d3.entries(baseColor));
-  let legendEntriesEnter = legendEntries.enter().append('div')
-    .attr('class', 'legendEntry')
-    .on('click', d => { highlightAll(d.key); d3.event.stopPropagation(); });
-  legendEntriesEnter.append('div')
-    .attr('class', 'legendLabel')
-    .text(d => d.key);
-  legendEntriesEnter.append('div')
-    .attr('class', 'colorbox')
-    .style('background-color', d => d.value);
+d3.text('map.svg', function (mapContent) {
+  d3.select('#map').html(mapContent);
+  d3.text('allTitles.csv', function (posterText) {
+    var posters = d3.dsvFormat('|').parse(posterText);
+    // A little data reshaping...
+    posters.sort((a, b) => a.title >= b.title ? 1 : -1);
+    window.posters = posters;
 
-  // Apply the colors to the map
-  let spaces = d3.select('svg').select('#Layer_2').selectAll('*')
-    .data(results[0], function (d) {
-      return d ? d.Space : this.id;
-    });
-  spaces.style('fill', d => baseColor[d.Group])
-    .style('cursor', 'pointer')
-    .attr('opacity', 1.0)
-    .on('click', function (d) { highlightAll(d.Group); d3.event.stopPropagation(); });
-  d3.select('body')
-    .on('click', function (d) { highlightAll(null); });
+    // Construct lookups for cleaner interaction code
+    var areaToTrack = {};
+    var trackToAreas = {};
+    var titleToArea = {};
+    posters.forEach(d => {
+      areaToTrack[d.Area] = d.Track;
 
-  // Create an author-name lookup
-  let authorAreas = {};
-  results[1].forEach(d => {
-    authorAreas[d['Name']] = d['Area'];
-  });
-
-  // Collect details about posters
-  let allTitles = {};
-  let posters = [];
-  let title = null;
-  let posterArea = null;
-  let details = [];
-  results[2].split('\n').forEach(d => {
-    if (!d) {
-      if (title !== null && !(title in allTitles)) {
-        posters.push({
-          title: title,
-          area: posterArea,
-          details: details
-        });
-        allTitles[title] = true;
+      if (trackToAreas.hasOwnProperty(d.Track) === false) {
+        trackToAreas[d.Track] = [];
       }
-      posterArea = null;
-      title = null;
-      details = [];
-    } else if (title === null) {
-      title = d;
-    } else {
-      details.push(d);
-      d.split(/\band\b|,/g).forEach(d => {
-        let author = d.trim();
-        posterArea = posterArea || authorAreas[author];
-      });
-    }
-  });
-  window.posters = posters;
-  posters.sort((a, b) => a.title >= b.title ? 1 : -1);
+      trackToAreas[d.Track].push(d.Area);
 
-  let posterDivs = d3.select('#titles').selectAll('div.poster').data(posters);
-  let posterDivsEnter = posterDivs.enter().append('div')
-    .attr('class', 'poster')
-    .on('click', d => { highlightAll(d.area); d3.event.stopPropagation(); });
-  posterDivsEnter.append('div')
-    .attr('class', 'colorbox')
-    .style('background-color', d => baseColor[d.area] || baseColor.Available);
-  posterDivsEnter.append('div')
-    .attr('class', 'title')
-    .text(d => d.title);
-  posterDivsEnter.append('div')
-    .attr('class', 'details')
-    .text(d => d.details.join('\n'));
+      titleToArea[d.Title] = d.Area;
+    });
+    window.areaToTrack = areaToTrack;
+    window.trackToAreas = trackToAreas;
+    window.titleToArea = titleToArea;
+
+    posters.forEach(d => {
+      areaToTrack[d.Area] = d.Track;
+    });
+    window.areaToTrack = areaToTrack;
+
+    // Create the color scale and the legend
+    var colorMap = {
+      'Scientific Computing': '#e2831e',
+      'Scientific Visualization': '#377eb8',
+      'Information Visualization': '#e41a1c',
+      'Biomedical Computation': '#ffff33',
+      'Imaging': '#4daf4a'
+    };
+
+    var legendEntries = d3.select('#legend').selectAll('div.legendEntry').data(d3.keys(trackToAreas));
+    var legendEntriesEnter = legendEntries.enter().append('div')
+      .attr('class', 'legendEntry')
+      .on('click', d => { highlightTrack(d); d3.event.stopPropagation(); });
+    legendEntriesEnter.append('div')
+      .attr('class', 'colorbox')
+      .style('background-color', d => colorMap[d]);
+    legendEntriesEnter.append('div')
+      .attr('class', 'legendLabel')
+      .text(d => d);
+
+    // Apply the colors to the map
+    var spaces = d3.select('svg').select('#Layer_2').selectAll('*')
+      .data(d3.entries(areaToTrack), function (d) {
+        return d ? d.key : this.id;
+      });
+    spaces.style('fill', d => colorMap[d.value])
+      .style('cursor', 'pointer')
+      .attr('opacity', 1.0)
+      .on('click', function (d) { highlightArea(d.key); d3.event.stopPropagation(); });
+
+    var titleDivs = d3.select('#titles').selectAll('div.poster').data(posters);
+    var titleDivsEnter = titleDivs.enter().append('div')
+      .attr('class', 'poster')
+      .on('click', d => { highlightTitle(d.Title); d3.event.stopPropagation(); });
+    titleDivsEnter.append('div')
+      .attr('class', 'colorbox')
+      .style('background-color', d => colorMap[d.Track]);
+    titleDivsEnter.append('div')
+      .attr('class', 'title')
+      .text(d => d.Title);
+    titleDivsEnter.append('div')
+      .attr('class', 'details')
+      .text(d => d.Details);
+  });
 });
